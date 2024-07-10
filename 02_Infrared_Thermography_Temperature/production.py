074aaf549e0f058a92a4314e6bb2bf7ae45ba6de
Ronaldo Canizales <rcanizales@uca.edu.sv>
2024-07-10 00:04:18 -0600

    Dia 01

---
+++ 02_Infrared_Thermography_Temperature/production.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+import csv
+
+# Load ML model
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+# Load input file
+unseen = []
+with open('input.txt', newline='\n') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        unseen = unseen + row
+
+# Use ML model
+unseen = [float(x) for x in unseen]
+unseen = np.array(unseen).reshape(1, -1)
+prediction = loaded_model.predict(unseen)
+
+# Save output file
+with open('output.txt', 'w', encoding='UTF8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([prediction[0]])
